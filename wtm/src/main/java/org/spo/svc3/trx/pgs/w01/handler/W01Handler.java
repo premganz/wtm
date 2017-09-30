634f31892dae64efb5a2c13b851696a5be7440f6
pg <misterprem@gmail.com>
2017-09-30 23:55:47 +0530

    pg fix

--- wtm/src/main/java/org/spo/svc3/trx/pgs/w01/handler/W01Handler.java
+++ wtm/src/main/java/org/spo/svc3/trx/pgs/w01/handler/W01Handler.java
@@ -5,6 +5,7 @@ import org.spo.ifs3.dsl.controller.DSLConstants.EventType;
 import org.spo.ifs3.dsl.controller.NavEvent;
 import org.spo.svc3.trx.pgs.w01.task.W0101;
 import org.spo.svc3.trx.pgs.w01.task.W0102;
+import org.spo.svc3.trx.pgs.w01.task.W0103;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,8 @@ public class W01Handler extends AbstractHandler{
    W0101 w0101;
    @Autowired
    W0102 w0102;
-   
+   @Autowired
+   W0103 w0103;
    
   public static final NavEvent EV_INIT_01 =  NavEvent.create(EventType.REFRESHPAGE,"01");
   public static final NavEvent EV_INIT_02 =  NavEvent.create(EventType.REFRESHPAGE,"02");
@@ -29,6 +31,7 @@ public class W01Handler extends AbstractHandler{
    public void configureChannel() {
 			taskChannel.put("01",w0101);
 			taskChannel.put("02",w0102);
+			taskChannel.put("03",w0103);
    }
    
    
